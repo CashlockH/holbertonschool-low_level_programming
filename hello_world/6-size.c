33a048da6d34dfe81d6f08a0622c0892483c5506
CashlockH <ilyashesenli48@gmail.com>
2024-02-23 13:37:46 +0000

    return values added to 5 and 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,4 +11,5 @@ printf("Size of an int: 4 byte(s)\n");
 printf("Size of a long int: 4 byte(s)\n");
 printf("Size of a long long int: 8 byte(s)\n");
 printf("Size of a float: 4 byte(s)\n");
+return(0);
 }
