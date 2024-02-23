4f22a48fc7ddff53932b0fa4ca90a7b60e4a5853
CashlockH <ilyashesenli48@gmail.com>
2024-02-23 13:32:50 +0000

    1-6 is done

---
+++ hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - printing with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+}
