bec6ac476589c99d07c30fced515e15196ab498c
CashlockH <ilyashesenli48@gmail.com>
2024-02-23 13:13:35 +0000

    as

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main-printing "Programming is like building a multilingual puzzle
+ * Return: it returns 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
