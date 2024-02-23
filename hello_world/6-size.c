987e82f9dff37fbf8fa9c86851ba91ba4a0a425b
CashlockH <ilyashesenli48@gmail.com>
2024-02-23 14:01:22 +0000

    as

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdio.h>
 /**
 * main - prints sizes of the types
 * Return: 0
@@ -8,7 +7,15 @@ int main(void)
 {
 printf("Size of a char: 1 byte(s)\n");
 printf("Size of an int: 4 byte(s)\n");
+switch (sizeof(void *))
+{
+case 4:
 printf("Size of a long int: 4 byte(s)\n");
+break;
+case 8:
+printf("Size of a long int: 8 byte(s)\n");
+break;
+}
 printf("Size of a long long int: 8 byte(s)\n");
 printf("Size of a float: 4 byte(s)\n");
 return (0);
