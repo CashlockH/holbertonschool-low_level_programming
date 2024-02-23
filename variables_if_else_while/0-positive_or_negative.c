dbce60f131c5bec6dba43bb04ff9fca606207ce2
CashlockH <ilyashesenli48@gmail.com>
2024-02-23 17:55:00 +0000

    fdasd

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - random value for n
- * Return: 0
+ * * main - random value for n
+ * Return: 0 
  */
 int main(void)
 {
@@ -11,10 +11,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("n is positive\n");
+printf("%d is positive\n", n);
 else if (n < 0)
-printf("n is negative\n");
+printf("%d is negative\n", n);
 else
-printf("n is zero\n");
+printf("%d is zero\n", n);
 return (0);
 }
