6958a7e252e9a7489a1b75e4fbacbf8847ec18ec
CashlockH <ilyashesenli48@gmail.com>
2024-02-23 18:29:54 +0000

    solving

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,10 +13,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 b = n % 10;
 if (b > 5)
-printf("Last digit of %d is %d and is greater than 5", n, b);
+printf("Last digit of %d is %d and is greater than 5\n", n, b);
 else if (b == 0)
-printf("Last digit of %d is 0", n);
+printf("Last digit of %d is 0\n", n);
 else
-printf("Last digit of %d is %d is less than 6 and not 0", n, b);
+printf("Last digit of %d is %d is less than 6 and not 0\n", n, b);
 return (0);
 }
