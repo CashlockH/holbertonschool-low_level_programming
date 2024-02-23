65ec938fcb32fd23632950fd5cf656430dcad59a
CashlockH <ilyashesenli48@gmail.com>
2024-02-23 17:44:31 +0000

    1-9 is done

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints 0-9 in string type
+ * Return: 0
+ */
+int main(void)
+{
+for (int i = 0 ; i <= 9 ; i++)
+{
+putchar('0' + i);
+}
+putchar('\n');
+}
