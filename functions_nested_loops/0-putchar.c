2d3d31ea9f2d6fc8aad9512ff0b8f40d91d76d50
CashlockH <ilyashesenli48@gmail.com>
2024-02-24 11:44:01 +0000

    1 is done

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,16 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "main.h"
 /**
  * main - prints 0-9 in string type with , and spaces
  * Return: 0
  */
 int main(void)
 {
-printf("_putchar");
+char *nese = "_putchar";
+int i = 0;
+for (; nese[i] != '\0' ; i++)
+{
+_putchar(nese[i]);
+}
+_putchar('\n');
 return (0);
 }
