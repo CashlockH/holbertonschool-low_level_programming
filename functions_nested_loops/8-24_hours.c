694edb1fe4c1fa43d46baa08aebaa6fd8e7fdf8e
CashlockH <ilyashesenli48@gmail.com>
2024-02-24 15:00:13 +0000

    asdf

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -5,24 +5,17 @@
  */
 void jack_bauer(void)
 {
-char hour[] = "00";
-char minute[] = "00";
-int d = 0, f = 0, g = 0, h = 0;
-for (; h <= 23 ; h++)
+int hour, minute;
+for (hour = 0; hour < 24; hour++)
 {
-g = 0;
-for (; g <= 9; g++)
+for (minute = 0; minute < 60; minute++)
 {
-f = 0;
-for (; f <= 5; f++)
-{
-d = 0;
-for (; d <= 9; d++)
-{
-_putchar(hour[0] + h);
-_putchar(hour[1] + g);
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
 _putchar(':');
-_putchar(minute[0] + f);
-_putchar(minute[1] + d);
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
 _putchar('\n');
-}}}}}
+}
+}
+}
