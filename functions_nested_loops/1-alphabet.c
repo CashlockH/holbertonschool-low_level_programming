8d761d5d255cbcbad8c98f410444272f46fcc4d9
CashlockH <ilyashesenli48@gmail.com>
2024-02-24 12:03:01 +0000

    2 is done

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *print_alphabet - prints alphabet in lowercase
+ *Return: nothing
+ */
+void print_alphabet(void)
+{
+char str = 'a';
+for (; str <= 'z' ; str++)
+_putchar(str);
+}
