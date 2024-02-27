fc422ede8b23125a7a9965da74a1f514d1849633
CashlockH <ilyashesenli48@gmail.com>
2024-02-27 15:56:51 +0000

    problem_solving

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,13 +10,15 @@ int i = 1;
 for (; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
-printf("%s ", "FizzBuzz");
+printf("%s", "FizzBuzz");
 else if (i % 3 == 0)
-printf("%s ", "Fizz");
+printf("%s", "Fizz");
 else if (i % 5 == 0)
-printf("%s ", "Buzz");
+printf("%s", "Buzz");
 else
-printf("%d ", i);
+printf("%d", i);
+if (i != 100)
+printf(" ");
 }
 printf("\n");
 return (0);
