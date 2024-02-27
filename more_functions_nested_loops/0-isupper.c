9c636197d3c00a660be5edc63191df957817634c
CashlockH <ilyashesenli48@gmail.com>
2024-02-27 05:54:44 +0000

    1 is done

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+/**
+ * _isupper - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isupper(int c)
+{
+if (c >= 60 && c <= 90)
+return (1);
+else 
+return (0);
+}
