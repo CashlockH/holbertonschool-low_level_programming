0a1b4ea9dda80672b9060fbb8cc7ecda5fb4be62
CashlockHolmes <ilyashesenli48@gmail.com>
2024-03-02 13:42:18 +0000

    8 is done

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - writes the character c to stdout
+ * @a: The character to print
+ * @n: The number of terms we want to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_array(int *a, int n)
+{
+int i = 0;
+for (; i < n; i++)
+{
+if (i < n - 1)
+printf("%d, ", a[i]);
+else
+printf("%d\n", a[i]);
+}
+}
