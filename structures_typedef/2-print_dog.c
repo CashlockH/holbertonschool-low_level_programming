5aa8fffe39b93c41bf9f8f0153b2aa656fa1a811
CashlockH <ilyashesenli48@gmail.com>
2024-03-03 06:55:58 +0000

    4.2 problem solving

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -16,6 +16,6 @@ if (d->name == NULL)
 d->name = " (nil)";
 if (d->owner == NULL)
 d->owner = " (nil)";
-printf("Name:%s\nAge:%f\nOwner:%s", d->name, d->age, d->owner);
+printf("Name: %s\nAge: %f\nOwner: %s", d->name, d->age, d->owner);
 }
 }
