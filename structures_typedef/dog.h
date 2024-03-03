51385f997e0df6a774e0e10dda50fb21eb0c087d
CashlockH <ilyashesenli48@gmail.com>
2024-03-03 07:02:03 +0000

    dog_t is added to dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: asdjfhsj
  * Description: Longer description
  */
-struct dog
+typedef struct dog
 {
 char *name;
 char *owner;
 float age;
-}dog_t;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* _SAMPLE_HEADER_H_ */
