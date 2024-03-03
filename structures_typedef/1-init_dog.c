601c8fe85801f91ef3f67c3ada9c6c6e844f9249
CashlockH <ilyashesenli48@gmail.com>
2024-03-03 06:39:45 +0000

    Problem solving for 4.1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "dog.h"
 /**
  * init_dog - Short description
@@ -9,6 +10,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+if (d == NULL)
+{
+printf("%s", "Ok");
+return;
+}
 (*d).name = name;
 (*d).age = age;
 (*d).owner = owner;
