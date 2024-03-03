3a189506200175d3ea445e24caa419cda535da98
CashlockH <ilyashesenli48@gmail.com>
2024-03-03 06:28:16 +0000

    4.1 is done

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+ * init_dog - Short description
+ * @d: First member
+ * @name: Second member
+ * @age: sdfa
+ * @owner: asdjfh
+ * Description: Longer description
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,5 +13,6 @@ char *name;
 char *owner;
 float age;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* _SAMPLE_HEADER_H_ */
