a2bb4dd9ce9bb780398363c47772c788679653f5
CashlockH <ilyashesenli48@gmail.com>
2024-03-05 03:10:59 +0000

    memset task is done

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - it changes the first n element of string s with char b.
+ * @s: The input string has to be changed
+ * @b: The character that will be added to string
+ * @n: The number of elements that will be changed
+ * Return: On success changed s string.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i = 0;
+while (i <= n)
+{
+s[i] = b;
+i++;
+}
+return (s);
+}
