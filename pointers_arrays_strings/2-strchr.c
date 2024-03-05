728e080d381c7f4ee284d231160291ab1426b703
CashlockH <ilyashesenli48@gmail.com>
2024-03-05 17:41:15 +0000

    strchr is done

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - makes the string beign started from the first occurance of character c
+ * @c: The character that is given as an input
+ * @s: string that is given as an input
+ * Return: If s string has c return new string, else NULL .
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strchr(char *s, char c)
+{
+int i = 0;
+char *b;
+while (s[i] != '\0')
+{
+if (s[i] == c)
+{
+b = (s + i);
+return (b);
+}
+i++;
+}
+return (NULL);
+}
