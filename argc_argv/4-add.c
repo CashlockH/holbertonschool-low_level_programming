8f694571e21d1a53f85cd82517577b596ac3c462
CashlockH <ilyashesenli48@gmail.com>
2024-03-10 15:22:38 +0400

    argc_argv.4 is done

---
+++ argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the answer of mu;tipilication of two integers
+ * @argc: the number of the arguments passed to the program
+ * @argv: the pointer points to array contains argumants
+ * Return: 0
+ * On error, 1
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int j = 0;
+	int add = 0;
+
+	if (argc > 1)
+	{
+		for (; i < argc ; i++)
+		{
+			for (j = 0; argv[i][j] != '\0' ; j++)
+			{
+				if (argv[i][j] >= 48 && argv[i][j] <= 57)
+				{
+					add = add + atoi(argv[i]);
+					break;
+				}
+				else
+				{
+					printf("%s\n", "Error");
+					return (1);
+				}
+			}
+		}
+	}
+	printf("%d\n", add);
+	return (0);
+}
