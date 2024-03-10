f5b26e7353c9a8a7504cf5fd6060571a93215c27
CashlockH <ilyashesenli48@gmail.com>
2024-03-10 14:01:29 +0400

    argc_argv.2 is done

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ * @argc: the number of the arguments passed to the program
+ * @argv: the pointer points to array contains argumants
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
