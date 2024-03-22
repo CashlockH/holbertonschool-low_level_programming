7781a718858982703157cc644b89f3955a92a90f
CashlockH <ilyashesenli48@gmail.com>
2024-03-22 13:45:20 +0000

    structer.4 is done

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * new_dog - function that inserts parameters to structure variable
+ * @name: the name of the structer variable
+ * @owner: the owner of the structer variable
+ * @age: the age value of the structer variable
+ * Return: the pointer that holds the adress of the structer variable
+ * On error NULL
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *nese;
+
+	nese = malloc(sizeof(dog_t));
+	if (nese == NULL)
+		return (NULL);
+	nese->name = name;
+	nese->owner = owner;
+	nese->age = age;
+	return (nese);
+}
