a84c5813c5bfed947ce413f89e9bd73995e1db10
CashlockH <ilyashesenli48@gmail.com>
2024-03-23 14:38:26 +0000

    function_pointer.1 is done

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - calls the function printing the array
+ * element by using function pointer
+ * @array: pointer to array which should be printed
+ * @size: size of the array
+ * @action: function pointer that is used to call the printing functions
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	while (i <= size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
