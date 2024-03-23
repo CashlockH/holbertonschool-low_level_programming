4462fe8ad0229cf17766076e303b62a3628e46b6
CashlockH <ilyashesenli48@gmail.com>
2024-03-23 14:12:33 +0000

    function_pointer.0 is done

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "fuction_pointers.h"
+/**
+ * print_name - calls the function printing the name
+ * @name: the name to be printed
+ * @f: the fuction pointer that is pointed to the function printing name
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ function_pointers/README.md
@@ -0,0 +1,1 @@
+This folder includes tasks involving the function pointer
