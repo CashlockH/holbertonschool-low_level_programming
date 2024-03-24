3c2347069ca1fb78364fc60c6ffced925513b59e
CashlockH <ilyashesenli48@gmail.com>
2024-03-24 11:14:07 +0000

    problem solving for function_pointer.3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,9 +11,9 @@
  */
 int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]);
-	char *op = argv[2];
-	int b = atoi(argv[3]);
+	int a;
+	char *op;
+	int b;
 	int result;
 
 	if (argc != 4)
@@ -21,6 +21,9 @@ int main(int argc, char *argv[])
 		printf("%s\n", "Error");
 		exit(98);
 	}
+	a = atoi(argv[1]);
+	op = argv[2];
+	b = atoi(argv[3]);
 	result = get_op_func(op)(a, b);
 	printf("%d\n", result);
 	return (0);
