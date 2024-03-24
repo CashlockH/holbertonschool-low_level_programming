84a9294821c6d5b8a23d7818a1d62d006ead31d2
CashlockH <ilyashesenli48@gmail.com>
2024-03-24 13:16:34 +0000

    misspelling in nil is corrected

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		argument = va_arg(ap, char *);
 		if (argument == NULL)
-			printf("%s", "nill");
+			printf("%s", "(nil)");
 		else
 			printf("%s", argument);
 		if (separator != NULL && i != n - 1)
