de59006e0bb5195124eff297efe54362fa8ed396
CashlockH <ilyashesenli48@gmail.com>
2024-04-02 07:55:39 +0000

    code is corrected for betty style

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,15 +1,21 @@
 #include <stddef.h>
 #include "lists.h"
 #include <stdio.h>
+/**
+ * print_list - prints the elements of the singly linked list
+ * @h: pointer to the first list element
+ * Return: the number of the members
+ * On error, 0
+ */
 size_t print_list(const list_t *h)
 {
 	int count = 0;
 
 	if (h == NULL)
-		return 0;
+		return (0);
 	if (h->str == NULL)
 	{
-		printf("%s\n", "[0], (nil)");
+		printf("%s\n", "[0] (nil)");
 	}
 	else
 		printf("[%d] %s\n", h->len, h->str);
