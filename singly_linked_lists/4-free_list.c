f06d17bbaa0d7ef8a4f0621f6852c2d2a014e6d8
CashlockH <ilyashesenli48@gmail.com>
2024-04-06 19:17:36 +0400

    list.4 is done

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "include.h"
+/**
+ * free_list - frees the members of the list
+ * @head: pointer to the first member of the list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	if (head->next != NULL)
+	{
+		free_list(head->next);
+		free(head);
+	}
+	else
+		free(head);
+}
