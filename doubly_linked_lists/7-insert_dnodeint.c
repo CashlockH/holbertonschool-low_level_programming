b0146d8b5fe0646219dc99987dbc4aa7dbacee49
CashlockH <ilyashesenli48@gmail.com>
2024-04-07 14:03:55 +0400

    return values are corrected

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -24,9 +24,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		i++;
 	}
 	if (!i)
-		add_dnodeint(h, n);
+		return (add_dnodeint(h, n));
 	else if(i && !tmp)
-		add_dnodeint_end(h,n);
+		return (add_dnodeint_end(h,n));
 	else
 	{
 		new->n = n;
