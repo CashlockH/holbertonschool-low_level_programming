ca17bbe1fc0d126ce58574983f2ad3819946b9e2
CashlockH <ilyashesenli48@gmail.com>
2024-04-07 23:41:54 +0400

    2 is done

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+/**
+ * append_text_to_file - afdssfsdf
+ * @filename: asdfsf
+ * @text_content: asfdsfs
+ *
+ * Return:asdf
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, w, len = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return (-1);
+
+	if (text_content)
+	{
+		while (*(text_content + len))
+			len++;
+		wed = write(fd, text_content, len);
+		if (wed < 0)
+			return (-1);
+	}
+	else
+		return (1);
+	close(fd);
+	return (1);
+}
