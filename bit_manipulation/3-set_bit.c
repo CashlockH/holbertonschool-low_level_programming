8db4f20234b1bdae54ad9113e389df98e8564520
CashlockH <ilyashesenli48@gmail.com>
2024-04-07 21:08:14 +0400

    code correction

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+/**
+ * set_bit - sets a bit at index
+ * @n: the decimal
+ * @index: the given index
+ * Return: 1
+ * On error, 0
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	long unsigned int d = 1 << index;
