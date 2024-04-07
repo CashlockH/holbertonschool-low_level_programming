a98cb3f20056d92aa1f11a5cbe7abfa751492d6a
CashlockH <ilyashesenli48@gmail.com>
2024-04-07 23:03:31 +0400

    code correction

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -6,7 +6,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char buf[500];
 	int fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return -1;
 	size_t n = read(fd, buf, letters);
+	if (n == -1)
+	{
+		close(fd);
+		return -1;
+	}
 	write(STDOUT_FILENO, buf, n);
 	close(fd);
 	return n;
