f176bed1991e0593b8e9a94fc7f07238b69a5024
CashlockH <ilyashesenli48@gmail.com>
2024-04-07 23:31:18 +0400

    0 is done

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -16,16 +16,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int n, w;
 
 	if (filename == NULL)
-		return 0;
+		return (0);
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		return(0);
+		return (0);
 	}
-
 	n = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, n);
 	close(fd);
