25d5c287deb3e6f8a600f2d4e17c07ec34682161
CashlockH <ilyashesenli48@gmail.com>
2024-04-07 11:34:43 +0400

    doublelist.4 is done

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+/**
+ * free_dlistint - frees the nodes of the list
+ * @head: pointer to the first node of the list
+ * Return: nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (head->next != NULL)
+		free_dlistint(head->next);
+	free(head);
+}
