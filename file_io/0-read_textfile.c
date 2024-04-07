f28ff969f6825457a0e2924f1507f8e6a8cf48a2
CashlockH <ilyashesenli48@gmail.com>
2024-04-07 23:13:35 +0400

    code correction

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,19 +4,36 @@
 #include <sys/stat.h>
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, n;
+	size_t written, n, total;
+	int fd;
 	char buf[500];
-
+	if (filename == NULL)
+	{
+		return 0;
+	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return -1;
-	n = read(fd, buf, letters);
+	{
+		return 0;
+	}
+	total = 0;
+
+	while ((n = read(fd, buf, sizeof(buf))) > 0)
+	{
+		written = write(STDOUT_FILENO, buf, n);
+		if (written == -1 || written != n)
+		{
+			close(fd);
+			return 0;
+		}
+		total += written;
+		if (total >= letters)
+			break;
+	}
+		close(fd);
 	if (n == -1)
 	{
-		close(fd);
-		return -1;
+		return 0;
 	}
-	write(STDOUT_FILENO, buf, n);
-	close(fd);
-	return n;
+	return total;
 }
