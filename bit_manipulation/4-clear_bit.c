4a7671d5cbeb29031b42acb4f2e36c65a1b869e7
CashlockH <ilyashesenli48@gmail.com>
2024-04-07 21:40:42 +0400

    code correction

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -10,15 +10,15 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	long unsigned int d = 1 << index;
-	long unsigned int a;
+	unsigned long int d = 1 << index;
+	unsigned long int a;
 
 	if (*n == 0)
 		return (-1);
 	if (index >= sizeof(unsigned long int) * CHAR_BIT)
 		return (-1);
 	a = *n ^ d;
-	if(a > *n)
+	if (a > *n)
 		*n = a - d;
 	else
 		*n = a;
