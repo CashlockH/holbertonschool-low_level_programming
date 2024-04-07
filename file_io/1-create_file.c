83d1a9a4c2edc259e166ddc5222b7b73787fcca9
CashlockH <ilyashesenli48@gmail.com>
2024-04-07 23:39:24 +0400

    0 is done

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -5,6 +5,12 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include "main.h"
+/**
+ * create_file - adfas
+ * @filename:fadf
+ * @text_content:asdfasf
+ * Return: 0
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
