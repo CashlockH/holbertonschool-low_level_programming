723a3e8d05acd5eb92f33f46512e5119f953f3a7
CashlockH <ilyashesenli48@gmail.com>
2024-04-07 21:09:00 +0400

    code correction

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -10,7 +10,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	long unsigned int d = 1 << index;
+	unsigned long int d = 1 << index;
 
 	if (index >= sizeof(unsigned long int) * CHAR_BIT)
 		return (-1);
