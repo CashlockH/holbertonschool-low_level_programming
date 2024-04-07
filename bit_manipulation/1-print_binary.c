39e4b6d43cb3a02dc8ba666d3ebb2e8e93450b56
CashlockH <ilyashesenli48@gmail.com>
2024-04-07 20:09:00 +0400

    code correction

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -3,6 +3,7 @@
 void print_binary(int num)
 {
 	int d = 1 << 10;
+	int i = 0;
 
 	if (num == 0)
 	{
@@ -12,8 +13,11 @@ void print_binary(int num)
 	while (d > 0)
 	{
 		if (num & d)
+		{
 			printf("1");
-		else
+			i = 1;
+		}
+		else if (i == 1)
 			printf("0");
 		d >>= 1;
 	}
