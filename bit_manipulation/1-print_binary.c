7bb16d6166626f9b2dbaaea9487202bf4c4980cc
CashlockH <ilyashesenli48@gmail.com>
2024-04-07 19:58:56 +0400

    bit.1 is done

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+void print_binary(int num)
+{
+	int d = 1 << 10;
+
+	if (num == 0)
+	{
+		printf("0");
+		return;
+	}
+	while (d > 0)
+	{
+		if (num & d)
+			printf("1");
+		else
+			printf("0");
+		d >>= 1;
+	}
+}
