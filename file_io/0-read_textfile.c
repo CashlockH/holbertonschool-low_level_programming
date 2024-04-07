cdc14df50a93f8bb92965109cd8f4c955b891a43
CashlockH <ilyashesenli48@gmail.com>
2024-04-07 23:28:49 +0400

    0 is done

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,39 +2,26 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int written, n;
-	size_t total;
+	char *buf;
 	int fd;
-	char buf[500];
+	int n, w;
+
 	if (filename == NULL)
-	{
 		return 0;
-	}
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		return 0;
+		return(0);
 	}
-	total = 0;
 
-	while ((n = read(fd, buf, sizeof(buf))) > 0)
-	{
-		written = write(STDOUT_FILENO, buf, n);
-		if (written == -1 || written != n)
-		{
-			close(fd);
-			return 0;
-		}
-		total += written;
-		if (total >= letters)
-			break;
-	}
-		close(fd);
-	if (n == -1)
-	{
-		return 0;
-	}
-	return total;
+	n = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, n);
+	close(fd);
+	return (w);
 }
