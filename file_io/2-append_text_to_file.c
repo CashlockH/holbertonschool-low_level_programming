cc2c5b48195a9805f642ec735b56c583d0a8da00
CashlockH <ilyashesenli48@gmail.com>
2024-04-07 23:48:46 +0400

    0 is done

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,12 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stddef.h>
-#include "main.h"
 /**
  * append_text_to_file - afdssfsdf
  * @filename: asdfsf
--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
-size_t read_textfile(const char *filename, size_t letters);
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-#endif /* MAIN_H */
+int append_text_to_file(const char *filename, char *text_content);
+#endif
