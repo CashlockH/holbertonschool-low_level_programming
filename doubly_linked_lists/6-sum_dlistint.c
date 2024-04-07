2ae89a0cbec90bfe9eb257aca306ce249142fac3
CashlockH <ilyashesenli48@gmail.com>
2024-04-07 12:39:04 +0400

    doublelist.6 is done

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * sum_dlistint - sums the data of the list nodes
+ * @head: the pointer to the first node
+ * Return: the sum
+ * On error, 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	if (head == NULL)
+		return (0);
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
