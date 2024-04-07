24597b94707177957907127dd8ab909f268c77e7
CashlockH <ilyashesenli48@gmail.com>
2024-04-07 14:44:37 +0400

    doublelist.8 is done

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,33 @@
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *tmp = *head;
+
+	while (tmp)
+	{
+		if (i == index)
+			break;
+		tmp = (tmp)->next;
+		i++;
+	}
+	if(!i)
+	{
+		if ((*head)->next)
+			*head = tmp->next;
+		else
+		{
+			free(*head);
+			return(1);
+		}
+		(*head)->prev = NULL;
+		free(tmp);
+		return(1);
+	}
+	else if(i != index)
+		return (-1);
+	tmp->prev->next = tmp->next;
+	tmp->next->prev = tmp->prev;
+	free(tmp);
+	return (1);
+}
